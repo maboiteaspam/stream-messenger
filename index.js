e8c6c713f9f7317a483c0cf126cd9e76aa0c101c
clement <maboiteaspam@wanadoo.fr>
2015-12-06 14:33:06 +0800

    make name optional

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var bubbled = require('bubbled')
 
+var k = 0;
+
 module.exports = function StreamMessenger(name, fnT, fnF) {
-  var stream = bubbled(name, ['message', 'error'], fnT, fnF);
+  var stream;
+
+  var args = [].slice.apply(arguments);
+  if (!name.substr && args.length<3) {
+    fnT = name;
+    fnF = fnT;
+    name = 'stream-msgr'+k
+    k++;
+  }
+  stream = bubbled(name, ['message', 'error'], fnT, fnF);
   stream.name = name;
   return stream;
 };
